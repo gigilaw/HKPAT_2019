14df1d6e92006c439f7219052b1aacb30085535a
gigilaw <gigi.law.14@gmail.com>
2019-05-03 01:32:34 +0800

    styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './App.css'
 import './component/navbar'
 import NavBar from './component/navbar'
+import Home from './component/home'
 
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
 			<header className="App-header">
-				<h1>Welcome to Hong Kong Hidden-A-Gender 2019!</h1>
+				<Home />
 			</header>
 		</div>
 	)
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
 	render() {
 		return (
 			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<a className="navbar-brand" href="#">
+				<a className="navbar-brand" href="/">
 					HKHAG2019
 				</a>
 				<button
