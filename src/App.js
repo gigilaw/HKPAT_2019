f0f561f2d54037cd2181d3ac7461b8e257783f80
gigilaw <gigi.law.14@gmail.com>
2019-05-03 00:35:07 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Welcome to Hong Kong Hidden-A-Gender 2019!</p>
+			</header>
+		</div>
+	)
+}
+
+export default App
