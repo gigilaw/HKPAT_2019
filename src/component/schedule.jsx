e244e75ba783ba7605f4ebcc80cff24a0b261f04
gigilaw <gigi.law.14@gmail.com>
2019-05-03 02:16:52 +0800

    img test

--- src/component/schedule.jsx
+++ src/component/schedule.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 
+const scheduleStyle = {
+	maxWidth: '70%',
+	margin: '5% auto',
+}
 class Schedule extends Component {
 	render() {
 		return (
 			<div className="container-fluid">
-				<img
-					src="http://oi64.tinypic.com/2gwexbo.jpg"
-					className="img-fluid"
-					alt="Responsive image"
-				/>
+				<div className="row">
+					<div className="col-sm" style={scheduleStyle}>
+						<img
+							src="http://oi64.tinypic.com/2gwexbo.jpg"
+							className="img-fluid"
+							alt="Responsive image"
+						/>
+					</div>
+				</div>
 			</div>
 		)
 	}
