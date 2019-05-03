a8739cde369f11f72bae1c47bba891fe87d6eaa7
gigilaw <gigi.law.14@gmail.com>
2019-05-03 17:55:39 +0800

    Refactoring

--- src/component/field.jsx
+++ src/component/field.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 
-const spanStyle = {
-	display: 'inline-block',
-	color: 'white',
-}
+const fields = [
+	{
+		name: 'Tai Hang Tung Recreation Ground',
+		address: '63 Boundary St, Prince Edward',
+	},
+	{ name: '大坑東遊樂場', address: '太子 界限街 36號' },
+]
 
-const divStyle = {
-	marginBottom: '40px',
-}
+const map =
+	'https://www.google.com/maps/place/Tai+Hang+Tung+Recreation+Ground/@22.3324231,114.1696872,16.07z/data=!4m8!1m2!2m1!1stai+hang+tung+recreation+playground!3m4!1s0x0:0x748f98cc502fa849!8m2!3d22.3279275!4d114.1710672?hl=en'
 
 class Field extends Component {
 	render() {
@@ -15,45 +17,30 @@ class Field extends Component {
 			<div className="container title">
 				<div className="row">
 					<div className="col-lg-6">
-						<div style={divStyle}>
+						<div className="divStyle">
 							<h1>Field Location</h1>
 						</div>
-						<h4>Tai Hang Tung Recreation Ground</h4>
-						<div style={divStyle}>
-							<span style={spanStyle}>
-								<a
-									className="inline"
-									href="https://www.google.com/maps/place/Tai+Hang+Tung+Recreation+Ground/@22.3324231,114.1696872,16.07z/data=!4m8!1m2!2m1!1stai+hang+tung+recreation+playground!3m4!1s0x0:0x748f98cc502fa849!8m2!3d22.3279275!4d114.1710672?hl=en"
-								>
-									<i class="fas fa-map-marker-alt fa-2x" />{' '}
-									<span style={spanStyle}>
-										<h5 className="inline address">
-											63 Boundary St, Prince Edward
-										</h5>
+						{fields.map(field => (
+							<div>
+								<h4>{field.name}</h4>
+								<div className="divStyle">
+									<span className="spanStyle">
+										<a className="inline" href={map}>
+											<i class="fas fa-map-marker-alt fa-2x" />
+											<span className="spanStyle">
+												<h5 className="inline address">{field.address}</h5>
+											</span>
+										</a>
 									</span>
-								</a>
-							</span>
-						</div>
-						<h4>大坑東遊樂場</h4>
-						<div style={divStyle}>
-							<span style={spanStyle}>
-								<a
-									className="inline"
-									href="https://www.google.com/maps/place/Tai+Hang+Tung+Recreation+Ground/@22.3324231,114.1696872,16.07z/data=!4m8!1m2!2m1!1stai+hang+tung+recreation+playground!3m4!1s0x0:0x748f98cc502fa849!8m2!3d22.3279275!4d114.1710672?hl=en"
-								>
-									<i class="fas fa-map-marker-alt fa-2x" />{' '}
-									<span style={spanStyle}>
-										<h5 className="inline address">太子 界限街 36號</h5>
-									</span>
-								</a>
-							</span>
-						</div>
-						<div style={divStyle}>
+								</div>
+							</div>
+						))}
+						<div className="divStyle">
 							<h4>Closest MTR Exit: Prince Edward A1</h4>
 						</div>
 					</div>
 					<div className="col-lg-6">
-						<div style={divStyle}>
+						<div className="divStyle">
 							<h1>Field Layout</h1>
 						</div>
 						<div>
