8a4ed7e2094d4ace7fd710cc0e9e975a6ef28d0f
gigilaw <gigi.law.14@gmail.com>
2019-05-03 13:19:08 +0800

    Contacts component update

--- src/component/contacts.jsx
+++ src/component/contacts.jsx
@@ -9,27 +9,33 @@ class Contact extends Component {
 					<h2>Tournament Directors:</h2>
 				</div>
 				<div className="container">
-					<div className="card-deck">
-						<div className="card justify-content-center">
-							<img
-								className="card-img-top"
-								src="http://i65.tinypic.com/2re031x.jpg"
-								alt="Derek Cheng"
-							/>
-							<div className="card-body">
-								<h4 className="card-title">Derek Cheng</h4>
-								<a href="https://wa.me/85255716292">
-									<i className="fab fa-whatsapp fa-2x" />
-								</a>
+					<div className="row">
+						<div className="card-deck">
+							<div className="card">
+								<img
+									className="card-img-top"
+									src="http://i65.tinypic.com/2re031x.jpg"
+									alt="Derek Cheng"
+								/>
+								<div className="card-body">
+									<h4 className="card-title">Derek Cheng</h4>
+									<a href="https://wa.me/85255716292">
+										<i className="fab fa-whatsapp fa-2x" />
+									</a>
+								</div>
 							</div>
-						</div>
-						<div className="card">
-							<img className="card-img-top" src="..." alt="Card image cap" />
-							<div className="card-body">
-								<h4 className="card-title">Tom Chung</h4>
-								<a href="https://wa.me/85292275540">
-									<i className="fab fa-whatsapp fa-2x" />
-								</a>
+							<div className="card">
+								<img
+									className="card-img-top"
+									src="http://i63.tinypic.com/21erk93.jpg"
+									alt="Card image cap"
+								/>
+								<div className="card-body">
+									<h4 className="card-title">Tom Chung</h4>
+									<a href="https://wa.me/85292275540">
+										<i className="fab fa-whatsapp fa-2x" />
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
