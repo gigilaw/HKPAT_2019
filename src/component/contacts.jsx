92bfb2b0dbdf4733238c00500040cdf857775f3c
gigilaw <gigi.law.14@gmail.com>
2019-05-03 12:49:52 +0800

    Contact page

--- src/component/contacts.jsx
+++ src/component/contacts.jsx
@@ -2,7 +2,40 @@ import React, { Component } from 'react'
 
 class Contact extends Component {
 	render() {
-		return <h1>Contact</h1>
+		return (
+			<div>
+				<div className="title container">
+					<h1>Contact Us</h1>
+					<h2>Tournament Directors:</h2>
+				</div>
+				<div className="container">
+					<div className="card-deck">
+						<div className="card justify-content-center">
+							<img
+								className="card-img-top"
+								src="http://i65.tinypic.com/2re031x.jpg"
+								alt="Derek Cheng"
+							/>
+							<div className="card-body">
+								<h4 className="card-title">Derek Cheng</h4>
+								<a href="https://wa.me/85255716292">
+									<i className="fab fa-whatsapp fa-2x" />
+								</a>
+							</div>
+						</div>
+						<div className="card">
+							<img className="card-img-top" src="..." alt="Card image cap" />
+							<div className="card-body">
+								<h4 className="card-title">Tom Chung</h4>
+								<a href="https://wa.me/85292275540">
+									<i className="fab fa-whatsapp fa-2x" />
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
 	}
 }
 
