51c944422eca55c591b3c7bb5634bae4ea82a6ff
gigilaw <gigi.law.14@gmail.com>
2019-05-04 11:24:12 +0800

    Sunday schedule update

--- src/component/schedule/sunSchedule.jsx
+++ src/component/schedule/sunSchedule.jsx
@@ -3,8 +3,16 @@ import React, { Component } from 'react'
 class sunSchedule extends Component {
 	render() {
 		return (
-			<div className="container-fluid">
-				<h1>Hello </h1>
+			<div className="col-xs" style={{ marginTop: '40px' }}>
+				<iframe
+					width="87%"
+					height="470px"
+					float="middle"
+					border="none"
+					scrolling="auto"
+					src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTXd2_tG0ff898hd3Etb4opovA15KwfCPD0O4lexKb0ZtOOUgJRoI3LYQg0tRUOwWKY_ldKrGLsDf0p/pubhtml?gid=1226252316&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false"
+				/>
+				/>
 			</div>
 		)
 	}
