1484509f2965de00dad35ed01fa859fd032c541e
gigilaw <gigi.law.14@gmail.com>
2019-05-05 20:34:45 +0800

    Fix error

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -52,7 +52,6 @@ class NavBar extends Component {
 									id="navbarDropdown"
 									role="button"
 									data-toggle="dropdown"
-									data-target=".navbar-collapse.show"
 									aria-haspopup="true"
 									aria-expanded="false"
 								>
