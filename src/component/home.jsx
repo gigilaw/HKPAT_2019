6af1ad9559feca651c40a59812390b76a7c3807d
gigilaw <gigi.law.14@gmail.com>
2019-05-05 17:50:30 +0800

    Home page update

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,4 +1,11 @@
 import React, { Component } from 'react'
+import HKUPA from '../logos/HKUPA.png'
+import SFOC from '../logos/SFOC.png'
+import LCSD from '../logos/LCSD.png'
+import Yikun from '../logos/Yikun.png'
+import CJ from '../logos/CJ.png'
+import TipTop from '../logos/TipTop.png'
+import title from '../logos/title.png'
 
 class Home extends Component {
 	render() {
@@ -7,23 +14,29 @@ class Home extends Component {
 				<div className="container title">
 					<div className="row">
 						<div className="col-12">
-							<h1>Heading</h1>
+							<img className="title img-fluid" src={title} alt="title" />
 						</div>
 					</div>
 					<div className="row">
 						<div className="col-4">
-							<p>Co-Organized By:</p>
+							<h5>Co-Organized By:</h5>
+							<img className="logo1" src={HKUPA} alt="SFOC" />
 						</div>
 						<div className="col-4">
-							<p>Co-Organized By:</p>
+							<h5>Organized By:</h5>
+							<img className="logo1" src={SFOC} alt="SFOC" />
 						</div>
 						<div className="col-4">
-							<p>Subvented By:</p>
+							<h5>Subvented By:</h5>
+							<img className="logo1" src={LCSD} alt="SFOC" />
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-12">
-							<p>Thank you to our sponsors!</p>
+						<div className="col-12 home">
+							<h5>Thank you to our sponsors!</h5>
+							<img className="Yikun" src={Yikun} alt="Yikun" />
+							<img className="CJ" src={CJ} alt="CJ" />
+							<img className="TipTop" src={TipTop} alt="TipTop" />
 						</div>
 					</div>
 					<div className="row">
