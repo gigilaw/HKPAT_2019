52bde5a970ff3878792450fbdafb2e76dfde5e87
gigilaw <gigi.law.14@gmail.com>
2019-05-05 14:23:32 +0800

    Social media icons

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -2,7 +2,47 @@ import React, { Component } from 'react'
 
 class Home extends Component {
 	render() {
-		return <h1>Hello</h1>
+		return (
+			<div>
+				<div className="container title">
+					<div className="row">
+						<div className="col-12">
+							<h1>Heading</h1>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-4">
+							<p>Co-Organized By:</p>
+						</div>
+						<div className="col-4">
+							<p>Co-Organized By:</p>
+						</div>
+						<div className="col-4">
+							<p>Subvented By:</p>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-12">
+							<p>Thank you to our sponsors!</p>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-12">
+							<h5>Follow HKUPA for more info about our future events!</h5>
+							<a href="https://www.facebook.com/hkupa/">
+								<i class="fab fa-facebook-square fa-2x icon" />
+							</a>
+							<a href="https://www.instagram.com/hkupa/?hl=en">
+								<i class="fab fa-instagram fa-2x icon" />
+							</a>
+							<a href="http://www.hkupa.com/">
+								<i class="fas fa-home fa-2x icon" />
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
 	}
 }
 
