cca8ab4af7dac746e7e819fae792a88385f9b4d3
gigilaw <gigi.law.14@gmail.com>
2019-05-05 21:29:57 +0800

    Embed Google form

--- src/component/spiritScores.jsx
+++ src/component/spiritScores.jsx
@@ -6,6 +6,18 @@ class Spirit extends Component {
 			<div>
 				<div className="container title">
 					<h2>Spirt Scores</h2>
+					<iframe
+						width="100%"
+						height="650px"
+						float="middle"
+						frameBorder="0"
+						marginheight="0"
+						marginwidth="0"
+						scrolling="auto"
+						src="https://docs.google.com/forms/d/e/1FAIpQLScN8EujkiqBhtTt-26JLilW41FVfgIb01Cew1IHq_S_DTF_kQ/viewform?embedded=true"
+					>
+						Loading...
+					</iframe>
 				</div>
 			</div>
 		)
